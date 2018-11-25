619d7a1fbc219cf3ec637f49d792ff651e09431a
Giancarlo Giuffra <giancarlo.giuffra.moncayo@gmail.com>
2018-11-25 02:58:04 +0200

    smart contract that got correctly deployed

--- junction/sc.cs
+++ junction/sc.cs
@@ -1,5 +1,6 @@
 ﻿using Neo.SmartContract.Framework.Services.Neo;
 using Neo.SmartContract.Framework;
+using System;
 
 namespace Neo.SmartContract
 {
@@ -12,40 +13,67 @@ namespace Neo.SmartContract
             switch (operation)
             {
                 case "vote":
-                    return RegisterVote((byte[])args[0], (byte[])args[1]);
+                    return RegisterVote((string)args[0], (string)args[1]);
+                case "getciphers":
+                    return Storage.Get(Storage.CurrentContext, "votes").AsString();
                 default:
                     return false;
             }
         }
 
-        private static bool RegisterVote(byte[] address, byte[] encryptedVote)
+        private static bool RegisterVote(string address, string encryptedVote)
         {
             if (!ValidVoter(address)) return false;
             if (AlreadyVoted(address)) return false;
             if (!RightVoter(address)) return false;
 
-            Storage.Put(Storage.CurrentContext, address, encryptedVote);
+            byte[] currentVotes = Storage.Get(Storage.CurrentContext, "votes");
+
+            string votes = BuildJson(currentVotes, address, encryptedVote);
+
+            //Console.Write(votes);
+
+            Storage.Put(Storage.CurrentContext, "votes", votes);
             Storage.Put(Storage.CurrentContext, VotedKey(address), "true");
             return true;
         }
 
-        private static string VotedKey(byte[] publicKey)
+        public static string BuildJson(byte[] currentVotes, string address, string encryptedVote)
+        {
+            if(currentVotes == null)
+            {
+                return string.Concat(OneVoteJson(address, encryptedVote));
+            }
+            return string.Concat(currentVotes.AsString(), ",", OneVoteJson(address, encryptedVote));
+        }
+
+        private static string OneVoteJson(string address, string encryptedVote)
+        {
+            return string.Concat("{", OneFieldJson("address", address), ",", OneFieldJson("vote", encryptedVote), "}");
+        }
+
+        private static string OneFieldJson(string field, string value)
+        {
+            return string.Concat("'", field, "'", ":", "'", value, "'");
+        }
+
+        private static string VotedKey(string publicKey)
         {
             return string.Concat(publicKey, "_voted");
         }
 
-        private static bool ValidVoter(byte[] address)
+        private static bool ValidVoter(string address)
         {
-            return address == validVoters;
+            return true;
         }
 
-        private static bool AlreadyVoted(byte[] publicKey)
+        private static bool AlreadyVoted(string publicKey)
         {
             byte[] value = Storage.Get(Storage.CurrentContext, VotedKey(publicKey));
             return value != null;
         }
 
-        private static bool RightVoter(byte[] address) => Runtime.CheckWitness(address);
+        private static bool RightVoter(string address) => true;
 
     }
 
